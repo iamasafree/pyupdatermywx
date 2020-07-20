01c86cde68d6f6c05297c77bd6842a99975c32d9
iamasafree <iamasafree@gmail.com>
2020-07-20 19:18:06 +0300

    deploy

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,2 +1,2 @@
 export PYUPDATER_FILESERVER_DIR=/home/asa/sites/deploy/pyupdatermywx
-scp -i C:/Users/asa/.ssh/id_rsa -v -o StrictHostKeyChecking=no ./pyu-data/deploy/* asa@fileserver-staging.asa.ubuntu:${PYUPDATER_FILESERVER_DIR}
+scp -i C:/Users/asa/.ssh/id_rsa ./pyu-data/deploy/* asa@fileserver-staging.asa.ubuntu:${PYUPDATER_FILESERVER_DIR}
