af70ac62a3d505e23a9f0c63b856b41a2a9b37fe
iamasafree <iamasafree@gmail.com>
2020-07-20 18:50:49 +0300

    deploy

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,3 +1,3 @@
 echo ${USER}
 export PYUPDATER_FILESERVER_DIR=/home/asa/sites/deploy/pyupdatermywx
-scp ./pyu-data/deploy/* asa@fileserver-staging.asa.ubuntu:${PYUPDATER_FILESERVER_DIR}
+sudo scp ./pyu-data/deploy/* asa@fileserver-staging.asa.ubuntu:${PYUPDATER_FILESERVER_DIR}
